ff2ce03510aa7e98768a79a54a5fafcdcd2a0a2a
Mohd Shahril <mohd_shahril_96@yahoo.com>
2018-02-27 21:47:09 +0800

    Auto detecting morse from user's input

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
+
 function MorseTranslator()
 {
-	var option = document.getElementById("translateOption").value;
 	var msgInput = document.getElementById("msgInput");
 	var msgOutput = document.getElementById("msgOutput");
 
-	if(option == "encode")
-	{
-		var encoded = morse.encode(msgInput.value);
-		msgOutput.value = encoded;
-	}
-	else if(option == "decode")
+	console.log(isMorseCode(msgInput.value));
+
+	if (isMorseCode(msgInput.value) == true)
 	{
 		var decoded = morse.decode(msgInput.value);
 		msgOutput.value = decoded;
 	}
+	else
+	{
+		var encoded = morse.encode(msgInput.value);
+		msgOutput.value = encoded;
+	}
+}
+
+function isMorseCode(str)
+{
+	for (var i=0; i < str.length; i++)
+		if (['/', '-', '.', ' '].indexOf(str.charAt(i)) == -1)
+			return false;
+	return true;
 }
